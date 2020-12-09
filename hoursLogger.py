885062ff00d3c23cef12d1b041a6bedc0f03d160
nicomwong <nicowong8@gmail.com>
2020-12-08 22:42:11 -0800

    added base logger

---
+++ hoursLogger.py
@@ -0,0 +1,134 @@
+import sys
+import json
+from enum import Enum
+import subprocess
+
+# Returns current date
+def getDate():
+	return subprocess.check_output("date").decode()[:-1]
+
+# Returns delta time in hours
+# Inputs are in HH::MM::SS 24-hour format
+# Output will assume delta time is less than 24 hours
+def timeDifference(startTime, endTime):
+	startHours = float(startTime[:2]) + float(startTime[3:5]) / 60
+	endHours = float(endTime[:2]) + float(endTime[3:5]) / 60
+	return (endHours - startHours) % 24.0	# Modulus so that if negative, it adds 24 hours
+
+# Possible States are:
+#	Idle: Waiting for a log to start
+#	Logging: Started logging
+class LogState(Enum):
+	loggingState = "Logging"
+	idleState = "Idle"
+
+# Check arguments
+if len(sys.argv) >= 2:
+	if not(
+		sys.argv[1] == "start" and len(sys.argv) == 3 or \
+		sys.argv[1] == "stop" or \
+		sys.argv[1] == "clear"
+		):
+
+		# Print invalid usage
+		print("Usage: python3 hoursLogger.py 'start description'|stop|clear")
+		sys.exit()
+
+# Check if clearing
+if sys.argv[1] == "clear":
+
+	# Open log file
+	with open('log.txt', 'r+') as logFile:
+		
+		# Copy log file to history file
+		with open('log_history.txt', 'a') as historyFile:
+
+			historyFile.write("----Logged hours cleared\t" + getDate() + '----\n')
+			historyFile.write( logFile.read() )
+			historyFile.write('\n')
+		
+		# Clear log file
+		logFile.truncate(0)
+		logFile.seek(0)
+
+		# Put in headers
+		logFile.write("Description\t\t\tStart Date\t\t\t\t\t\tEnd Date\t\t\t\t\t\tHours\n")
+
+		# Reset state
+		with open('state.txt', 'w') as stateFile:
+			
+			json.dump(LogState.idleState.value, stateFile)
+			stateFile.truncate()
+
+	# Exit program
+	sys.exit()
+
+# Init. state var
+state = ""
+
+# Open state file in read/write
+stateFile = open('state.txt', 'r+')
+
+# Get current state
+state = json.load(stateFile)
+
+stateFile.seek(0)	# Reset file ptr
+
+# Valid actions are:
+#	start: Starts logging if state is Idle
+#	stop: Ends logging if state is Logging
+
+# Open logging file in append mode
+with open('log.txt', 'a+') as logFile:
+
+	# Start
+	if sys.argv[1] == "start":
+
+		if state == "Idle":
+			
+			# Insert description (padded or clipped to size 20)
+			descripColSize = 20	# column size of description
+			description = sys.argv[2][:descripColSize - 4] + ' ' * max(descripColSize - 4 - len(sys.argv[2]), 0)
+			logFile.write(description + ' ' * 4)
+
+			# Insert start date
+			logFile.write( getDate() + "\t" )
+
+			# Update state
+			json.dump(LogState.loggingState.value, stateFile)
+			stateFile.truncate()	# Cuts down to correct size
+
+		else:
+			
+			# Print invalid
+			print("Cannot start logging. Current state is already Logging")
+	
+	elif sys.argv[1] == "stop":
+
+		if state == "Logging":
+			
+			# Calc. delta time in hours
+			logFile.seek( logFile.tell() - 29)	# Go back to beginning of the line
+			startTime = logFile.readline()[11:19]	# Read the startTime
+			endDate = getDate()
+			endTime = endDate[11:19]	# Get the endTime
+			deltaTime = timeDifference(startTime, endTime)
+
+			# Insert end date
+			logFile.write( endDate + "\t" )
+
+			# Insert hours difference
+			logFile.write('{:.2f}'.format(deltaTime) + '\n')
+
+			# Update state
+			json.dump(LogState.idleState.value, stateFile)
+			stateFile.truncate()	# Cuts down to correct size
+
+		else:
+			# Print invalid
+			print("Cannot end logging. Current state is Idle")
+
+	else:
+		# Print invalid command
+		print("Usage: python3 hoursLogger.py 'start description'|stop|clear")
+		sys.exit()
